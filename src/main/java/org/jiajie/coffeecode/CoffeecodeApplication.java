86c63eef5d63e4f660fd227b1b25077a42609d95
jay <jay.jia@livenaked.com>
2019-10-27 20:35:15 +0800

    update ignore

---
+++ src/main/java/org/jiajie/coffeecode/CoffeecodeApplication.java
@@ -0,0 +1,13 @@
+package org.jiajie.coffeecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jiajie/coffeecode/CoffeecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jiajie.coffeecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
