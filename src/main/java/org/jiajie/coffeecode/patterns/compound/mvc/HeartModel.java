66086a67e75062805cc31119bdca270394dc5518
jay <jay.jia@livenaked.com>
2019-12-03 17:02:43 +0800

    upgrade 1. MVC, initial

---
+++ src/main/java/org/jiajie/coffeecode/patterns/compound/mvc/HeartModel.java
@@ -0,0 +1,34 @@
+package org.jiajie.coffeecode.patterns.compound.mvc;
+
+public class HeartModel implements HeartModelInterface, Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public int getHeartRate() {
+        return 0;
+    }
+
+    @Override
+    public void registerObserver(BeatObserver o) {
+
+    }
+
+    @Override
+    public void removeObserver(BeatObserver o) {
+
+    }
+
+    @Override
+    public void registerObserver(BPMObserver o) {
+
+    }
+
+    @Override
+    public void removeObserver(BPMObserver o) {
+
+    }
+}
