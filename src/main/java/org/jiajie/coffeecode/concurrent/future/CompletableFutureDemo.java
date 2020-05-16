bb7ab19b6b9d7c8c90d217cca4323f0f0b9ba208
jay <jay.jia@livenaked.com>
2020-05-16 22:03:00 +0800

    1.new-class Gate

---
+++ src/main/java/org/jiajie/coffeecode/concurrent/future/CompletableFutureDemo.java
@@ -0,0 +1,16 @@
+package org.jiajie.coffeecode.concurrent.future;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureDemo {
+
+    /**
+     * thenAccept() example
+     */
+    public void completableFutureThenAcceptDemoMethod(){
+//        CompletableFuture.supplyAsync(() -> {
+//
+//        }
+//        );
+    }
+}
--- src/main/java/org/jiajie/coffeecode/concurrent/single/Gate.java
+++ src/main/java/org/jiajie/coffeecode/concurrent/single/Gate.java
@@ -1,4 +1,7 @@
 package org.jiajie.coffeecode.concurrent.single;
 
 public class Gate {
+    private int counter = 0;
+    private String name = "Nobody";
+    private String address = "Nowhere";
 }
