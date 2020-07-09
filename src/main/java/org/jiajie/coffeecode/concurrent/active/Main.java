ec989b3a305204b32f28dc35861f4bb102779f35
jay <jay.jia@livenaked.com>
2020-07-09 15:06:03 +0800

    1.new-package active

---
+++ src/main/java/org/jiajie/coffeecode/concurrent/active/Main.java
@@ -0,0 +1,9 @@
+package org.jiajie.coffeecode.concurrent.active;
+
+/**
+ * 测试程序行为的类
+ *
+ * @author jay
+ */
+public class Main {
+}
---
+++ src/main/java/org/jiajie/coffeecode/concurrent/active/MakerClientThread.java
@@ -0,0 +1,9 @@
+package org.jiajie.coffeecode.concurrent.active;
+
+/**
+ * 发出"生成字符串"请求的线程
+ *
+ * @author jay
+ */
+public class MakerClientThread {
+}
