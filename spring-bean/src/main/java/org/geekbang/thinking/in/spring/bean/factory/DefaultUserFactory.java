46c8ef63e9ed3d18f675e6666e4d3bc1fd282ae4
jay <jay.jia@livenaked.com>
2020-11-06 11:16:11 +0800

    1.new-class SpecialBeanInstantiationDemo.java

--- spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.InitializingBean;
  * @author jay
  */
 public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
+
+    public void initUserFactory(){
+        System.out.println("自定义初始化方法 initUserFactory() : UserFactory 初始化中...");
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
 
@@ -17,5 +22,14 @@ public class DefaultUserFactory implements UserFactory, InitializingBean, Dispos
     @Override
     public void destroy() throws Exception {
 
+    }
+
+    public void doDestroy(){
+        System.out.println("自定义销毁方法 doDestroy() : UserFactory 销毁中...");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("当前 DefaultUserFactory 对象正在被垃圾回收...");
     }
 }
