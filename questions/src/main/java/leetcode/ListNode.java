1c9d1fbf66ab81b40dca3a5b55549d3d1200b935
jay.jia <jay.jia@livenaked.com>
2020-11-21 10:29:21 +0800

    1. new-class Solution148.java

---
+++ questions/src/main/java/leetcode/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * @author jie
+ * @date 2020/11/21 9:12 AM
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
