38f04ebba0b9b1874e8d7bb31db6dd37cf4f59cb
jay <jay.jia@livenaked.com>
2020-12-01 10:08:05 +0800

    1.new-class Solution34.java

---
+++ questions/src/main/java/leetcode/middle/Solution34.java
@@ -0,0 +1,33 @@
+package leetcode.middle;
+
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * @author jay
+ */
+public class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
